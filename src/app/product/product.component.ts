3daf96a39629c8296369d00cc22197e537ebd279
Wergo09 <gentletron@mail.ru>
2023-04-18 21:34:39 +0300

    update_repo

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DatabaseService } from './../services/database.service';
 
 @Component({
   selector: 'app-product',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  constructor(
+    private route: ActivatedRoute,
+    private databaseService: DatabaseService
+  ) {}
+
+  
 }
